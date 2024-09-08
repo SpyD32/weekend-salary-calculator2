3824bcb842f63866b26cb1b686307a9b89cd2098
Michael Silas <michael.silas30@gmail.com>
2024-09-08 13:13:53 -0500

    created table head and made border

--- client.js
+++ client.js
@@ -1,7 +1,9 @@
 let newRow = 'employee';
 let total = 'totalCost';
 let clear = 'clearForm'
-let html = document.getElementById
+//this will be part of the trigger function
+//to add the row for new employee
+let html = document.getElementById('firstNameBlock', 'lastNameBlock').innerHTML
 
 function insertRow() {
     console.log(insertRow);
