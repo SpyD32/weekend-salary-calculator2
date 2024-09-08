ce0c0ff42da4d31262dcf7ea38b123d62667f159
Michael Silas <michael.silas30@gmail.com>
2024-09-08 12:57:57 -0500

    added varialble for html

---
+++ client.js
@@ -0,0 +1,11 @@
+let newRow = 'employee';
+let total = 'totalCost';
+let clear = 'clearForm'
+let html = document.getElementById
+
+function insertRow() {
+    console.log(insertRow);
+    return insertRow;
+}
+
+// A new employee row should be added to the table.
