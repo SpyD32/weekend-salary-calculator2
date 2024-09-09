b9c75f19360a930d651dce5f38a195ec49db7664
Michael Silas <michael.silas30@gmail.com>
2024-09-09 18:17:11 -0500

    function test past

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ let total = 'totalCost';
 let clear = 'clearForm'
 //this will be part of the trigger function
 //to add the row for new employee
-let html = document.getElementById('firstNameBlock', 'lastNameBlock').innerHTML
+let html = document.getElementById('tableBlock').innerHTML
 
 function insertRow() {
-    console.log(insertRow);
+    //testing function to make sure it works.
+    console.log('row created', insertRow);
     return insertRow;
 }
+
+//console.log("testing" , Hello, World);
+
 
 // A new employee row should be added to the table.
