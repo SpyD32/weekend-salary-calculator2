a318fcbf7122775939e88189c1bf34ecde1edff5
Michael Silas <michael.silas30@gmail.com>
2024-09-09 20:08:53 -0500

    added borders to tr and td

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-let newRow = 'employee';
+const newPerson = document.getElementById("submitButton")
 let total = 'totalCost';
 let clear = 'clearForm'
 //this will be part of the trigger function
 //to add the row for new employee
 //let html = document.getElementById('employeeRow').innerHTML
 
-function newEmployeeRow() {
+function newEmployee() {
     //testing function to make sure it works.
     //function works. Need to attach new employee row 
-    console.log('row created', newEmployeeRow);
+    console.log('row created', newEmployee);
     return newEmployeeRow;
     button.addEventListener(onClick());
+   
+    
 }
+
