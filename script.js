57ef35879438bd443df9edd1de9053245ee03343
Michael Silas <michael.silas30@gmail.com>
2024-09-14 13:35:52 -0500

    added backticks to template literals to fix error

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ const newPerson = document.getElementById("submitButton")
 let total = 'totalCost';
 let clear = 'clearForm'
 let firstName = document.getElementById('firstName').value;
-const employeeTable = document.querySelector('table');
+let lastName = document.getElementById('lastName').value;
+let idNumber = document.getElementById('id').value;
+let jobTitle = document.getElementById('jobTitle').value;
+let annualSalary = document.getElementById('annualSalary').value;
+const employeeTable = document.querySelector('table').value;
 
-employeeTable.innerHTML +=
-    <tr>
-        <td>${firstName}</td>
-        <td>${lastName}</td>
-        <td>${id}</td>
-        <td>${jobTitle}</td>
-        <td>${annualSalary}</td>
-        <td><button class="deleteBtn">Delete</button></td>
-        
-    </tr>
+employeeTable.innerHTML += `
+ <tr>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${id}</td>
+    <td>${jobTitle}</td>
+    <td>${annualSalary}</td>
+    <td><button class="deleteBtn">Delete</button></td>  
+</tr>
+`;
 //this will be part of the trigger function
-//to add the row for new employee
+//to add the row for new employee$
 //let html = document.getElementById('employeeRow').innerHTML
 
 function newEmployee(event) {
@@ -24,10 +28,9 @@ function newEmployee(event) {
     console.log('row created', newEmployee);
     return newEmployeeRow;
     button.addEventListener(onClick());
+    event.preventDefault(newEmployee)
    
     
 }
 
-function submitForm() {
-    
-}
+//function submitForm() {
