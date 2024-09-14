3e35bb77b1f1d0078766a645c4b7ea32dc7b13b8
Michael Silas <michael.silas30@gmail.com>
2024-09-14 12:55:32 -0500

    adding a value to all inputs

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ let clear = 'clearForm'
 const employeeTable = document.querySelector('table');
 
 employeeTable.innerHTML +=
-
+    <tr>
+        <td>${firstName}</td>
+        <td>${firstName}</td>
+        <td>${firstName}</td>
+        <td>${firstName}</td>
+        <td>${firstName}</td>
+        <td>${firstName}</td>
+        
+    </tr>
 //this will be part of the trigger function
 //to add the row for new employee
 //let html = document.getElementById('employeeRow').innerHTML
