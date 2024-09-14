5d7ba5fe4d5ab9f2888fdc0c9c6fe947a91580e7
Michael Silas <michael.silas30@gmail.com>
2024-09-14 12:45:32 -0500

    adding an id to <table> and adding innerHTML to add to the DOM

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const newPerson = document.getElementById("submitButton")
 let total = 'totalCost';
 let clear = 'clearForm'
+const employeeTable = document.querySelector('table');
+
+employeeTable.innerHTML +=
+
 //this will be part of the trigger function
 //to add the row for new employee
 //let html = document.getElementById('employeeRow').innerHTML
 
-function newEmployee() {
+function newEmployee(event) {
     //testing function to make sure it works.
     //function works. Need to attach new employee row 
     console.log('row created', newEmployee);
@@ -15,3 +19,6 @@ function newEmployee() {
     
 }
 
+function submitForm() {
+    
+}
