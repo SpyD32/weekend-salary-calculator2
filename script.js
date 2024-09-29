96db72f3dfb33755fbef43cd93feb5daa73d3feb
Michael Silas <michael.silas30@gmail.com>
2024-09-28 21:30:04 -0500

    updates are not working in the test, I have tested with Alex and Alex confirms that it works

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-const newPerson = document.getElementById("submitButton")
-let total = 'totalCost';
-let clear = 'clearForm'
 let firstName = document.getElementById('firstName').value;
+
 let lastName = document.getElementById('lastName').value;
+
 let id = document.getElementById('ID').value;
+
 let jobTitle = document.getElementById('jobTitle').value;
+
 let annualSalary = document.getElementById('annualSalary').value;
+
 const employeeTable = document.getElementById('empTable')
 
-//this will be part of the trigger function
-//to add the row for new employee$
-//let html = document.getElementById('employeeRow').innerHTML
 
 function newEmployee(event) {
     //testing function to make sure it works.
     //function works. Need to attach new employee row 
     event.preventDefault()
+    console.log('submitButton');
 
-event.target.innerHTML = 'click to clear row'    
+    
+ 
 const newPerson = document.getElementById("submitButton")
-total = 'totalCost';
+
+// let currentMonthly = Number(document.querySelector("#monthlyAmount").innerText);
+// console.log('this is the current monthly' , currentMonthly);
+
+
 firstName = document.getElementById('firstName').value;
+
+
 lastName = document.getElementById('lastName').value;
+
 id = document.getElementById('ID').value;
+
 jobTitle = document.getElementById('jobTitle').value;
-annualSalary = document.getElementById('annualSalary').value;
+
+annualSalary = document.getElementById('annualSalary').value; 
+monthlyTotal += parseInt(annualSalary) 
+
 const employeeTable = document.getElementById('empTable')
     employeeTable.innerHTML += `
  <tr>
@@ -33,18 +45,34 @@ const employeeTable = document.getElementById('empTable')
     <td>${id}</td>
     <td>${jobTitle}</td>
     <td>${annualSalary}</td>
-    <td><button onClick="deleteButton()" class="deleteBtn">Delete</button></td> 
+    <td><button onclick="deleteButton(event)" class="deleteBtn">Delete</button></td> 
         
         
 </tr>
+
 `;
-function removeRow(event) {
-    event.target.closest('tr').remove();
+document.getElementById('firstName').value = ''
+document.getElementById('lastName').value = ''
+document.getElementById('ID').value = ''
+document.getElementById('jobTitle').value = ''
+document.getElementById('annualSalary').value = ''
+
+let monthly = document.getElementById('total-monthly')
+monthly.innerHTML = ''
+monthly.innerHTML += `
+${monthlyTotal}`
+
+
 }
 
+let monthlyTotal = 0 
 
 
-}
-
-
-//function submitForm() {
+function deleteButton(event) {
+    console.log(event);
+console.log('deleting row');
+    let parentDiv= event.target.parentElement;
+    parentDiv.closest("tr").remove();
+    
+    
+  }
